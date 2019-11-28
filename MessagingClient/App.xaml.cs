c0a1c721eab34134b92b745b128cc9d62acaea9b
Maxim Gorbatyuk <maxim.gorbatyuk@outlook.com>
2019-11-28 08:12:55 +0600

    + Minor changes

--- MessagingClient/App.xaml.cs
+++ MessagingClient/App.xaml.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using ServiceBusHelper;
 
 namespace MessagingClient
 {
@@ -13,5 +8,13 @@ namespace MessagingClient
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Здесь мы передаем настройки главному окну, и так главное окно не знает, откуда ему провайднули настройки.
+            // Здесь уже можно читать из файла или еще откуда-то.
+            // https://stackoverflow.com/questions/47370784/start-two-windows-in-sequence-from-app-xaml-cs-wpf
+            var mainWindow = new MainWindow(new ClientSettingsDto());
+            mainWindow.Show();
+        }
     }
 }
---
+++ MessagingClient/BrokerMessageSender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+using ServiceBusHelper;
+
+namespace MessagingClient
+{
+    public class BrokerMessageSender
+    {
+        private readonly SBClientManager _messageClient;
+
+        public BrokerMessageSender(SBClientManager messageClient)
+        {
+            _messageClient = messageClient ?? throw new ArgumentNullException(nameof(messageClient));
+        }
+
+        public Task SendFile(string filename)
+        {
+            var file = new BrokeredMessage(new FileStream(filename, FileMode.Open));
+            var fileMessage = new FileMessage(Path.GetFileName(filename), file);
+
+            return Task.Factory.StartNew(() => _messageClient.Send(fileMessage));
+        }
+    }
+}
