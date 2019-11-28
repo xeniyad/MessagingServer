cb227428847c687284da9ae8df8e91a17d3dcb6c
Maxim Gorbatyuk <maxim.gorbatyuk@outlook.com>
2019-11-28 08:20:21 +0600

    + refactor

--- MessagingClient/BrokerMessageSender.cs
+++ MessagingClient/BrokerMessageSender.cs
@@ -17,10 +17,24 @@ namespace MessagingClient
 
         public Task SendFile(string filename)
         {
-            var file = new BrokeredMessage(new FileStream(filename, FileMode.Open));
+            var file = new BrokeredMessage(OpenFile(filename));
             var fileMessage = new FileMessage(Path.GetFileName(filename), file);
 
             return Task.Factory.StartNew(() => _messageClient.Send(fileMessage));
+        }
+
+        private FileStream OpenFile(string filename)
+        {
+            try
+            {
+                return new FileStream(filename, FileMode.Open);
+            }
+            catch (Exception exception)
+            {
+                // Создавая подобные кастомные эксепшены с человеко-читаемым текстом, мы упрощаем работу с логами.
+                // ПРи этом не забываем класть пойманное исключение из API в наше создаваемое как innerException.
+                throw new InvalidOperationException($"Cannot open file {filename}", exception);
+            }
         }
     }
 }
